7c341dccaf3830527e1deab2890c9abf34b91e1f
VISHAL-PC\Lunar <lunargamesindia@gmail.com>
2018-01-21 20:57:04 +0530

    Added: Project Files

---
+++ Ludo/Assets/Ludo/Editor/RenameGameobjects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RenameGameobjects : Editor {
+
+    [MenuItem("GameObject/Rename")]
+    public static void Rename()
+    {
+        GameObject[] selected = Selection.gameObjects;
+        for(int i= 0; i< selected.Length; i++)
+        {
+            selected[i].name = "point_"+selected[i].transform.GetSiblingIndex().ToString();
+        }
+    }
+
+}
